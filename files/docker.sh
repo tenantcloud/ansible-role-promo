3bdbb3dc5d624031ca2bde079d11eaaaf86791e4
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-02-18 09:36:29 +0200

    feature: changed the role under docker flow

---
+++ files/docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
+
+# Added path to 'tcctl' in PATH variable
+cd "$(dirname "$0")"/../../ || exit
+PATH=$(pwd)/cli:$PATH
+
+docker run --rm -i --mount source=tc-www-nfsmount,target=/app tenantcloud/composer \
+  composer install --no-interaction --prefer-dist --optimize-autoloader
+docker run --rm -i --network=tc-www-network --mount source=tc-www-nfsmount,target=/app \
+  -w /app --entrypoint "/app/sh/install/minio.sh"  minio/mc:latest
+$(command -v yarn) ci
+$(command -v yarn) release
+docker-compose exec -T www ln -s /app/resources/web /app/public/web
