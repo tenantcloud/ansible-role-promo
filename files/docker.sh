3674dc4a1b88faa63ad387cb32d5c168240b5301
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-05-17 21:46:39 +0300

    modify: changed deploy script, added tagger

--- files/docker.sh
+++ files/docker.sh
@@ -6,9 +6,9 @@
 cd "$(dirname "$0")"/../../ || exit
 PATH=$(pwd)/cli:$PATH
 
-docker-compose exec -T www composer install --no-interaction --prefer-dist --optimize-autoloader
+docker-compose exec -T app composer install --no-interaction --prefer-dist --optimize-autoloader
 docker run --rm -i --network=tc-www-network --mount source=tc-www-nfsmount,target=/app \
   -w /app --entrypoint "/app/sh/install/minio.sh"  minio/mc:latest
 $(command -v yarn) ci
 $(command -v yarn) release
-docker-compose exec -T www ln -s /app/resources/web /app/public/web
+docker-compose exec -T app ln -s /app/resources/web /app/public/web
